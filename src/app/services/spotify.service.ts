ca34fc393a8f4272b08d49b98cb9a01e12e55869
John Rodriguez <john.rodriguez.25@hotmail.com>
2018-07-18 16:47:00 -0500

    Se agrega metodo para obtener artista

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -17,4 +17,12 @@ export class SpotifyService {
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
     
   }
+
+  getArtista() {
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQASNLSii5HIJpTDJR5VTHwV0yMcwyJRWB6PD9R3Yyai_EvDew408SNFUC_joqV0Me4oZqfAvx_jDG1MCEg'
+    })
+
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
+  }
 }
